c4db9f3436c8826abfab435a2528b4eb2fb07be5
Abel <abelgoytom77@gmail.com>
2026-03-01 15:00:49 +0000

    fix(fundraising): update error handling for video thumbnail to use x-on directive for improved compatibility

--- resources/views/member/partials/fundraising-popup.blade.php
+++ resources/views/member/partials/fundraising-popup.blade.php
@@ -33,7 +33,7 @@
                                 <div class="absolute inset-0 cursor-pointer group" @click="videoPlaying = true">
                                     <img :src="thumbnailUrl"
                                          class="w-full h-full object-cover"
-                                         @error="$el.src = 'https://img.youtube.com/vi/' + videoId + '/hqdefault.jpg'">
+                                         x-on:error="$el.src = 'https://img.youtube.com/vi/' + videoId + '/hqdefault.jpg'">
                                     <div class="absolute inset-0 bg-black/25 group-hover:bg-black/15 transition-colors"></div>
                                     <div class="absolute inset-0 flex items-center justify-center">
                                         <div class="w-16 h-16 rounded-full flex items-center justify-center shadow-2xl transition-transform group-hover:scale-110 group-active:scale-95"
